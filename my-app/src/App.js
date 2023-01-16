dbf3ece9578ae50cd0ad8317dd33363970cb5b80
BuildTools <unconfigured@null.spigotmc.org>
2023-01-15 20:24:24 -0500

    Installed React Router Dom

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,17 @@ import Header from "./components/Header.jsx"
 import Searchbar from "./components/Searchbar.jsx"
 import Searchbutton from "./components/Searchbutton.jsx"
 import Bookcard from "./components/Bookcard.jsx"
+import Favorites from './Pages/Favorites'
 import Information from "./components/Information"
 import React, { useState } from 'react';
+import {Routes, Route} from "react-router-dom"
 
 
 
 
+
+
+let bookStartInt=0;
 
 export default function App() {
 
@@ -18,14 +23,15 @@ let bookAuthor = ["author1"];
 let imageSrc = ["img1"];
 
 const[books, setBooks] = useState([]);
-const[bookStartInt, setStart] = useState([0]);
+//const[bookStartInt, setStart] = useState([0]);
+
 const[currentPage, setPage] = useState([1]);
 const[searchTerm,setSearchTerm]=useState("")
 const[favoriteBooks,setFavorite]=useState([])
 const[resultNumber,setResultNumber]=useState(0)
 
 const BOOKURL = "https://www.googleapis.com/books/v1/volumes?q=";
-const MAXRESULTS = "&maxResults=12"
+const MAXRESULTS = "&maxResults=6"
 const STARTINDEXSTRING = "&startIndex="
 
 
@@ -38,7 +44,7 @@ function handleSearch(){
 
 
 function backendCallAPI(){
-  
+   
 console.log(BOOKURL + searchTerm + MAXRESULTS + STARTINDEXSTRING + bookStartInt)
   fetch(BOOKURL + searchTerm + MAXRESULTS + STARTINDEXSTRING + bookStartInt).then((response)=>{
     return response.json();
@@ -46,7 +52,7 @@ console.log(BOOKURL + searchTerm + MAXRESULTS + STARTINDEXSTRING + bookStartInt)
 
 
 setBooks(data.items)
-
+console.log(data.items)
 console.log(data.totalItems)
 //setResultNumber(books.totalItems);
 
@@ -58,7 +64,8 @@ console.log(data.totalItems)
 
 
 function goRight(){
-setStart(Number.parseInt(bookStartInt) + 6);
+//setStart(Number(bookStartInt) + 6);
+bookStartInt=bookStartInt+6;
 //setPage(++pageCount);
 backendCallAPI();
 
@@ -66,13 +73,81 @@ backendCallAPI();
 
 function goLeft(){
   if(bookStartInt>=5){
-setStart(Number.parseInt(bookStartInt) - 6);
+//setStart(Number(bookStartInt) - 6);
+bookStartInt=bookStartInt-6;
 //setPage(--pageCount);
 backendCallAPI();
   }
 }
 
-const RenderBooks=()=>{
+
+
+
+
+
+function addFavorite(book){
+  if(favoriteBooks.indexOf(book)==-1){
+    setFavorite([...favoriteBooks,book])
+  }else{
+    let arr=favoriteBooks;
+    let index=favoriteBooks.indexOf(book);
+  
+ 
+    let result=[]
+    for(let i=0;i<arr.length;i++){
+      if(i != index){
+        result.push(arr[i])
+      }
+    }
+
+
+    setFavorite(result)
+    
+  }
+
+}
+
+
+
+  return (
+  
+ 
+  <div className='App'>
+  
+    <Routes> 
+<Route path="/" element={ <Header/> }/>
+<Route path="/favorites" element={ <Favorites/> }/>
+   
+ </Routes>
+
+  </div>
+ 
+  
+  )
+}
+
+const Favorite=({favoriteBooks})=>{
+
+  return (<>
+{favoriteBooks.length>0 &&
+  favoriteBooks.map((item)=><h1>{item}</h1>)
+
+
+}
+  </>)
+}
+const Result=({books,favoriteBooks,addFavorite})=>{
+
+  return <div className='container'> 
+      {books.length>0 &&
+      <>
+        <RenderBooks favoriteBooks={favoriteBooks} addFavorite={addFavorite} books={books} />
+        <Favorite favoriteBooks={favoriteBooks} />
+      </>
+      }
+     </div>
+ }
+ const RenderBooks=({books,addFavorite,favoriteBooks})=>{
   const arr=[]
  for(let i=0;i<5;i++){
   if(books[i] && books[i].volumeInfo){
@@ -80,42 +155,29 @@ const RenderBooks=()=>{
     title={books[i].volumeInfo.title?books[i].volumeInfo.title:"Unknown Title" }
     author={books[i].volumeInfo.authors?books[i].volumeInfo.authors[0]:"Unknown Author"}
     image={books[i].volumeInfo.imageLinks?books[i].volumeInfo.imageLinks.thumbnail:""}
+    self={books[i].selfLink}
+    addFavorite={addFavorite}
+    marked={favoriteBooks.indexOf(books[i].selfLink)>-1}
 />  )
   }
  }
  return arr;
 
 }
-const Result=()=>{
 
- return <div className='container'> 
-     {books.length>0 &&
-       <RenderBooks />
-     }
-    </div>
-}
+const RenderFavorites=({favoriteBooks})=>{
+ const arr=[];
+ for(let i=0; i<favoriteBooks.length; i++){
+  arr.push(<Bookcard
+      title={favoriteBooks[i] }
+      author={favoriteBooks[i]}
+      image={favoriteBooks[i]}
+      self={favoriteBooks[i]}
+  />  )    
 
-function addFavorite(){
-setFavorite([])
-
-
-}
+ }
 
 
 
 
-
-
-
-
-// When scrolling back and forth through the book results, it cannot go back to the first result (because 'useState' is delayed by one???)
-
-  return (
-  <div className='App'>
-    <Header/>
-    <Searchbar handleSearch={handleSearch} goLeft={goLeft} goRight={goRight} searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-     {/*<Information resultNumber={5} />*/}
-    <Result/>
-  </div>
-  )
 }
--- my-app/src/components/Bookcard.jsx
+++ my-app/src/components/Bookcard.jsx
@@ -1,4 +1,8 @@
-function Bookcard({title,author,image,addFavorite}){
+function Bookcard({title,author,image,self,addFavorite,marked}){
+
+ 
+ 
+  
     return(
       <div className="card">
         <h3 >{title}</h3>  
@@ -6,11 +10,11 @@ function Bookcard({title,author,image,addFavorite}){
         
         <p className="author"> {author} </p>
         <img className="thumbnail" src={image}/>
-        <button onClick={addFavorite}> Add to Favorite </button>    
+        <button onClick={()=>addFavorite(self)}>{marked?"Remove from":"Add to"}Favorite </button>    
         </div>
       
     
     )
     }
-    
+
     export default Bookcard;
