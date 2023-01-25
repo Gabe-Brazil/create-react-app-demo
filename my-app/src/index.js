f5ec6f1e5156528b1cc6fa3814f530d5523c30c5
BuildTools <unconfigured@null.spigotmc.org>
2023-01-25 13:25:58 -0500

    Installed Firebase, began signup page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,33 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGmDegw0sB6kVzXdogmKUll5FLHoXQlr0",
+  authDomain: "book-finder-66e68.firebaseapp.com",
+  projectId: "book-finder-66e68",
+  storageBucket: "book-finder-66e68.appspot.com",
+  messagingSenderId: "787011487126",
+  appId: "1:787011487126:web:df5cfa41590bb22e0589b7",
+  measurementId: "G-LK49VBTXR9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
